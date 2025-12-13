81fdd4ecbe60b701383adbdc6f0658b1ab21643a
TensuraWEL <user@example.com>
2025-12-13 04:49:18 -0500

    chore: Remove example skills, keep only Skill Forge
    - Removed ExampleCommonSkill, ExampleExtraSkill, ExampleIntrinsicSkill
    - Removed ExampleUniqueSkill, ExampleUltimateSkill
    - Removed ExampleRace
    - Cleaned up AllSkills.java and AllRaces.java
    - Removed example textures and translations

--- src/main/java/com/github/b4ndithelps/tenforge/registry/race/AllRaces.java
+++ src/main/java/com/github/b4ndithelps/tenforge/registry/race/AllRaces.java
@@ -1,29 +1,22 @@
 package com.github.b4ndithelps.tenforge.registry.race;
 
 import com.github.b4ndithelps.tenforge.Tenforge;
-import com.github.b4ndithelps.tenforge.race.ExampleRace;
-import com.github.manasmods.tensura.registry.race.TensuraRaces;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.IForgeRegistry;
-import net.minecraftforge.registries.RegisterEvent;
+import com.github.manasmods.tensura.race.Race;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
 
-@Mod.EventBusSubscriber(modid = Tenforge.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+/**
+ * Race registry for Tenforge.
+ * Register custom races here if needed.
+ */
 public class AllRaces {
 
-    // Define the Race that you want to add here
-    public static final ResourceLocation EXAMPLE_RACE = new ResourceLocation(Tenforge.MODID, "example_race");
+    public static final DeferredRegister<Race> RACES = 
+            DeferredRegister.create(Race.getRegistryKey(), Tenforge.MODID);
 
-    /**
-     * Make sure that you register the race, otherwise it will not show up correctly in the selection menu
-     * @param event
-     */
-    @SubscribeEvent
-    public static void register(RegisterEvent event) {
-        event.register(((IForgeRegistry) TensuraRaces.RACE_REGISTRY.get()).getRegistryKey(), helper -> {
-            helper.register("example_race", new ExampleRace());
-        });
+    public static void register(IEventBus modEventBus) {
+        RACES.register(modEventBus);
     }
 
+    // Add custom races here as needed
 }
--- src/main/java/com/github/b4ndithelps/tenforge/registry/skill/AllSkills.java
+++ src/main/java/com/github/b4ndithelps/tenforge/registry/skill/AllSkills.java
@@ -1,12 +1,6 @@
 package com.github.b4ndithelps.tenforge.registry.skill;
 
-
 import com.github.b4ndithelps.tenforge.Tenforge;
-import com.github.b4ndithelps.tenforge.ability.skill.common.ExampleCommonSkill;
-import com.github.b4ndithelps.tenforge.ability.skill.extra.ExampleExtraSkill;
-import com.github.b4ndithelps.tenforge.ability.skill.intrinsic.ExampleIntrinsicSkill;
-import com.github.b4ndithelps.tenforge.ability.skill.ultimate.ExampleUltimateSkill;
-import com.github.b4ndithelps.tenforge.ability.skill.unique.ExampleUniqueSkill;
 import com.github.b4ndithelps.tenforge.ability.skill.unique.SkillForgeSkill;
 import com.github.manasmods.manascore.api.skills.ManasSkill;
 import com.github.manasmods.manascore.api.skills.SkillAPI;
@@ -15,59 +9,24 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 /**
- * This file is responsible for registering all the skills with the official Tensura Mod.
- * It works by using a single DeferredRegister, which is based off of the ManasCore Skill Registry.
- * Any skill you create must go into that registry.
- *
- * All Skill types can go in here.
+ * Skill registry for Tenforge.
+ * Register all custom skills here.
  */
 public class AllSkills {
 
-    // Here is that deferred register I was talking about. You don't need to change it at all
-    public static DeferredRegister<ManasSkill> skillRegistry = DeferredRegister.create(SkillAPI.getSkillRegistryKey(), Tenforge.MODID);
+    public static final DeferredRegister<ManasSkill> SKILLS = 
+            DeferredRegister.create(SkillAPI.getSkillRegistryKey(), Tenforge.MODID);
 
     public static void register(IEventBus modEventBus) {
-        skillRegistry.register(modEventBus);
+        SKILLS.register(modEventBus);
     }
 
-    // Here is where the skills are registered. To add another skill, simply duplicate the RegistryObject, and change it to match your skill defined in ../ability/skill/
-
-    //   =================
-    //   | Common Skills |
-    //   =================
-    public static final RegistryObject<ExampleCommonSkill> EXAMPLE_COMMON =
-            skillRegistry.register("example_common", ExampleCommonSkill::new);
-
-    //   ====================
-    //   | Intrinsic Skills |
-    //   ====================
-    public static final RegistryObject<ExampleIntrinsicSkill> EXAMPLE_INTRINSIC =
-            skillRegistry.register("example_intrinsic", ExampleIntrinsicSkill::new);
-
-    //   ================
-    //   | Extra Skills |
-    //   ================
-    public static final RegistryObject<ExampleExtraSkill> EXAMPLE_EXTRA =
-            skillRegistry.register("example_extra", ExampleExtraSkill::new);
-
-    //   =================
-    //   | Unique Skills |
-    //   =================
-    public static final RegistryObject<ExampleUniqueSkill> EXAMPLE_UNIQUE =
-            skillRegistry.register("example_unique", ExampleUniqueSkill::new);
+    // ==================== Unique Skills ====================
     
-    // Skill Forge - Analyze, breakdown, and synthesize skills (inspired by Raphael/Great Sage from Tensura)
+    /**
+     * Skill Forge - Break down and create skills using energy
+     * Inspired by Raphael/Great Sage from Tensura
+     */
     public static final RegistryObject<SkillForgeSkill> SKILL_FORGE =
-            skillRegistry.register("skill_forge", SkillForgeSkill::new);
-
-    //   =====================
-    //   | Resistance Skills |
-    //   =====================
-
-
-    //   ===================
-    //   | Ultimate Skills |
-    //   ===================
-    public static final RegistryObject<ExampleUltimateSkill> EXAMPLE_ULTIMATE =
-            skillRegistry.register("example_ultimate", ExampleUltimateSkill::new);
+            SKILLS.register("skill_forge", SkillForgeSkill::new);
 }
